440f75f5a3c598ddc7ba56ffc88ca0d8cf23b3ab
maomao <maoaixing@136.com>
2023-01-30 20:14:06 +0800

    hotfix-commit

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println("测试git");
         System.out.println("hot-fix");
         System.out.println("分支冲突");
+        System.out.println("hot-fix  修改");
     }
 }
